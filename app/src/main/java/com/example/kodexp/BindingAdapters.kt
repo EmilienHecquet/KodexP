202a5053ab9adb9206439b212323805e461be67d
Nathan <nathan.montois@gmail.com>
2023-11-02 18:50:59 +0100

    commit push

--- app/src/main/java/com/example/kodexp/BindingAdapters.kt
+++ app/src/main/java/com/example/kodexp/BindingAdapters.kt
@@ -1,2 +1,12 @@
 package com.example.kodexp
 
+import android.view.View
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.LiveData
+
+@BindingAdapter("app:visibility")
+fun setViewVisibility(view: View, isVisible: LiveData<Boolean>?) {
+    isVisible?.value?.let {
+        view.visibility = if (it) View.VISIBLE else View.GONE
+    }
+}
--- app/src/main/java/com/example/kodexp/ui/opening/OpeningFragment.kt
+++ app/src/main/java/com/example/kodexp/ui/opening/OpeningFragment.kt
@@ -1,21 +1,23 @@
+package com.example.kodexp.ui.opening
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.kodexp.databinding.FragmentOpeningBinding
+import com.example.kodexp.databinding.FragmentDashboardBinding
 
 class OpeningFragment : Fragment() {
 
-    private lateinit var binding: FragmentOpeningBinding
+    private lateinit var binding: FragmentDashboardBinding
     private lateinit var viewModel: OpeningViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentOpeningBinding.inflate(inflater, container, false)
+        binding = FragmentDashboardBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(OpeningViewModel::class.java)
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
